6e851e23113f24ce9e29ca62c248f76c1e43117d
Matheus Tavares dos Santos <matheustavaressantos17@gmail.com>
2020-11-18 01:03:34 -0300

    Primeiro portfolio

---
+++ www/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Matheus Tavares dos Santos</title>
+
+
+    <!-- Meta Data -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Matheus Tavares dos Santos" />
+    <meta name="description" content="Portifólio de trabalhos realizados e formação de Matheus Tavares dos Santos" />
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/styles/style-dark.css" />
+
+
+</head>
+
+<body class="bg-triangles">
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="preloader__wrap">
+            <div class="circle-pulse">
+                <div class="circle-pulse__1"></div>
+                <div class="circle-pulse__2"></div>
+            </div>
+            <div class="preloader__progress"><span></span></div>
+        </div>
+    </div>
+
+    <main class="main">
+        <div class="container gutter-top">
+            <!-- Header -->
+            <header class="header box">
+                <div class="header__left">
+                    <div class="header__photo">
+                        <img class="header__photo-img" src="assets/img/matheus.jpeg" alt="Matheus Tavares dos Santos">
+                    </div>
+                    <div class="header__base-info">
+                        <h4 class="title titl--h4">Matheus Tavares dos Santos</h4>
+                        <div class="status">Desenvolvedor WEB e Cadista</div>
+                        <ul class="header__social">
+                            <li>
+                                <a href="https://www.instagram.com/tavares.matheus17/">
+                                    <i class="font-icon icon-instagram"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/theteustav">
+                                    <i class="font-icon icon-github"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/user/TheMatheus1713/featured?view_as=subscriber">
+                                    <i class="font-icon icon-youtube"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/Matheus17056743">
+                                    <i class="font-icon icon-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/matheus.tavares.39108">
+                                    <i class="font-icon icon-facebook"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="header__right">
+                    <ul class="header__contact">
+                        <li>
+                            <span class="overhead">
+                                Email
+                            </span>
+                            matheustavares@contato.com.br
+                        </li>
+                        <li>
+                            <span class="overhead">Telefone</span><a
+                                href="https://api.whatsapp.com/send?phone=5563992xxxx95">+55 (63) 992xx-xx95 </a>
+                        </li>
+                    </ul>
+                    <ul class="header__contact">
+
+                        <li>
+                            <span class="overhead">
+                                Endereço
+                            </span>
+                            Araguaína - TO
+                        </li>
+                    </ul>
+                </div>
+            </header>
+
+            <div class="row sticky-parent">
+                <!-- Sidebar nav -->
+                <aside class="col-12 col-md-12 col-lg-2">
+                    <div class="sidebar box sticky-column">
+                        <ul class="nav">
+                            <li class="nav__item">
+                                <a class="active" href="index.php">
+                                    <i class="icon-user"></i>
+                                    Sobre Mim
+                                </a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="resume.php">
+                                    <i class="icon-file-text"></i>
+                                    Currículo
+                                </a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="works.php">
+                                    <i class="icon-codesandbox"></i>
+                                    Portifólio
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+
+                <!-- Conteúdo do site -->
+                <div class="col-12 col-md-12 col-lg-10">
+                    <div class="box box-content">
+                        <!-- Sobre Mim  -->
+                        <div class="pb-0 pb-sm-2">
+                            <h1 class="title title--h1 first-title title__separate">Sobre Mim</h1>
+                            <p>Olá, sou Matheus Tavares, tenho 27 anos e resido em Araguaína-TO. Sou formado em Sistemas
+                                de Informação e estou em busca de novos desafios e conhecimento.
+                                Atualmente trabalho como Cadista Topográfico na empresa RCA e estudo programação. </p>
+
+
+                            <div>
+
+                                <!-- Clients -->
+                                <div class="mt-4">
+                                    <h2 class="title title--h3">Clientes</h2>
+
+                                    <div class="swiper-container js-carousel-clients">
+                                        <div class="swiper-wrapper">
+                                            <!-- Item client -->
+                                            <div class="swiper-slide">
+                                                <a href="#">
+                                                    <img src="assets/img/apple.jpg" style="background-color: #FFF;"
+                                                        alt="apple" /></a>
+                                            </div>
+
+                                            <!-- Item client -->
+                                            <div class="swiper-slide">
+                                                <a href="#"><img src="assets/img/facebook.jpg"
+                                                        style="background-color: #FFF;" alt="facebook" /></a>
+                                            </div>
+                                            <!-- Item client -->
+                                            <div class="swiper-slide">
+                                                <a href="#"><img src="assets/img/microsoft.jpg"
+                                                        style="background-color: #FFF;" alt="IFTO Logo" /></a>
+                                            </div>
+                                            <!-- Item client -->
+                                            <div class="swiper-slide">
+                                                <a href="#">
+                                                    <img src="assets/img/samsung.jpg" style="background-color: #FFF;"
+                                                        alt="samsung" /></a>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="swiper-pagination"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Footer -->
+                            <footer class="footer">
+                                © Matheus Tavares dos Santos
+                            </footer>
+                        </div>
+                    </div>
+                </div>
+    </main>
+
+    <!-- SVG masks -->
+    <svg class="svg-defs">
+        <clipPath id="avatar-box">
+            <path
+                d="M1.85379 38.4859C2.9221 18.6653 18.6653 2.92275 38.4858 1.85453 56.0986.905299 77.2792 0 94 0c16.721 0 37.901.905299 55.514 1.85453 19.821 1.06822 35.564 16.81077 36.632 36.63137C187.095 56.0922 188 77.267 188 94c0 16.733-.905 37.908-1.854 55.514-1.068 19.821-16.811 35.563-36.632 36.631C131.901 187.095 110.721 188 94 188c-16.7208 0-37.9014-.905-55.5142-1.855-19.8205-1.068-35.5637-16.81-36.63201-36.631C.904831 131.908 0 110.733 0 94c0-16.733.904831-37.9078 1.85379-55.5141z" />
+        </clipPath>
+        <clipPath id="avatar-hexagon">
+            <path
+                d="M0 27.2891c0-4.6662 2.4889-8.976 6.52491-11.2986L31.308 1.72845c3.98-2.290382 8.8697-2.305446 12.8637-.03963l25.234 14.31558C73.4807 18.3162 76 22.6478 76 27.3426V56.684c0 4.6805-2.5041 9.0013-6.5597 11.3186L44.4317 82.2915c-3.9869 2.278-8.8765 2.278-12.8634 0L6.55974 68.0026C2.50414 65.6853 0 61.3645 0 56.684V27.2891z" />
+        </clipPath>
+    </svg>
+
+    <div class="back-to-top"></div>
+
+    <!-- JavaScripts -->
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/plugins.min.js"></script>
+    <script src="assets/js/common.js"></script>
+
+</body>
+
+</html>
